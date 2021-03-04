07e03601bfed24c9c3d15bbe2db1b6fdbd682138
Malte Brandy <malte.brandy@maralorn.de>
2021-03-04 19:40:31 +0100

    Add attribute unison which builds from source via cabal

--- nix/default.nix
+++ nix/default.nix
@@ -1,7 +1,11 @@
-{ sources ? import ./sources.nix, pkgs ? import sources.nixpkgs {} }:
+{ sources ? import ./sources.nix, pkgs ? import sources.nixpkgs { config.allowBroken = true; } }:
 
 {
   unison-ucm = pkgs.callPackage ./ucm.nix {};
+
+  unison = pkgs.callPackage ./unison-cabal.nix {
+    inherit sources;
+  };
 
   vim-unison = pkgs.callPackage ./vim-unison.nix {
     inherit (pkgs.vimUtils) buildVimPluginFrom2Nix;
