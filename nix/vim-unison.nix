4e709bb043efe88006a223b8d078e2e802cd3067
Cody Allen <ceedubs@gmail.com>
2021-11-03 09:54:12 -0700

    Update Unison src and add support for vim completion

--- nix/vim-unison.nix
+++ nix/vim-unison.nix
@@ -1,6 +1,12 @@
-{ buildVimPluginFrom2Nix, unisonSrc }:
+{ buildVimPluginFrom2Nix, curl, jq, unisonSrc }:
 
 buildVimPluginFrom2Nix {
   name = "vim-unison";
   src = unisonSrc + "/editor-support/vim";
+
+  patchPhase = ''
+    substituteInPlace autoload/unison.vim \
+      --replace '"curl"' '"${curl}/bin/curl"' \
+      --replace '"jq"' '"${jq}/bin/jq"'
+  '';
 }
