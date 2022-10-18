a83964fd4e500ca54a332a6821a5f83d2512b6ec
Cody Allen <ceedubs@gmail.com>
2022-10-18 09:04:07 -0400

    dev-shell: special-case x86 darwin

--- nix/dev-shell.nix
+++ nix/dev-shell.nix
@@ -1,8 +1,15 @@
-{ darwin, fzf, glibcLocales, git, gmp, less, lib, makeWrapper, mkShell, ncurses, ormolu, stack, stdenv, symlinkJoin, zlib }:
+{ darwin, fzf, glibcLocales, git, gmp, less, lib, libiconv, makeWrapper, mkShell, ncurses, ormolu, stack, stdenv, symlinkJoin, system, zlib }:
 
 let
-  nativeLibs = lib.optionals stdenv.isDarwin (with darwin.apple_sdk.frameworks; [ Cocoa CoreServices ]);
-  stack-wrapped = symlinkJoin {
+  isX86Darwin = system == "x86_64-darwin";
+  nativeLibs = lib.optionals stdenv.isDarwin (with darwin.apple_sdk.frameworks; [ Cocoa CoreServices libiconv ]);
+
+  # Admittedly I'm flailing a bit here. --nix seems to work well on other
+  # platforms, but when I try it with x86 darwin, it generates assembly that is
+  # invalid for the platform. It seems like some sort of clang or llvm mismatch
+  # or something, but I can't figure it out.
+  stack-wrapped = if isX86Darwin then stack else
+  symlinkJoin {
     name = "stack";
     buildInputs = [ makeWrapper ];
     paths = [ stack ];
