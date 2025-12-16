e56d5cd2ae89144d3686e40cbe2c7d68806ffdc4
Noon van der Silk <noonsilk+-noonsilk@gmail.com>
2025-12-16 13:09:00 +0000

    Include a src dir when building snake

--- nix/build-share-project.nix
+++ nix/build-share-project.nix
@@ -54,6 +54,6 @@ Compile functions from a project hosted on Unison Share into executables.
 in
   buildFromTranscript {
     inherit pname version compiledHash meta;
-
+    src = ./.;
     transcript = builtins.toFile "${pname}-compile-transcript-${version}.md" transcriptContents;
   }
