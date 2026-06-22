a2060ebbef2c3cbdcb46d8afd5349b9fe242b88f
Greg Pfeil <greg@technomadic.org>
2026-06-22 08:50:07 -0600

    Update to the latest Unison release
    This release is _not_ in the cache, but it removes significant complexity from
    the Nix environment, as you can see from the flake.lock changes.

--- flake.nix
+++ flake.nix
@@ -23,7 +23,7 @@
       ## NB: Before upgrading this, make sure the release you upgrade to is
       ##     pinned in the cache (https://app.cachix.org/cache/unison#pins) for
       ##     all supported systems.
-      url = "github:unisonweb/unison/release/1.1.0";
+      url = "github:unisonweb/unison/release/1.3.0";
     };
   };
 
